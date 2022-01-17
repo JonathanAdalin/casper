03bde8678dd5619b82fc8341c09a13483d9230a2
jonathanadalin@gmail.com <angelo@Angelos-Computer.local>
2022-01-16 22:31:40 -0500

    Add some color

--- casper.rb
+++ casper.rb
@@ -1,31 +1,47 @@
+require 'colorize'
 require 'ruby-progressbar'
 
 NUM_QUESTIONS_TOTAL = 12
 NUM_QUESTIONS_PER_PERIOD = 6
 
-# Fake variables for testing
-SCENARIO_TIME_S = 3
-QUESTION_TIME_S = 15
-BREAK_TIME_S = 3
+# Fake variables for testing # TODO Remove
+WELCOME_TIME_S = 4
+SCENARIO_TIME_S = 4
+QUESTION_TIME_S = 10
+BREAK_TIME_S = 10
 
-# Real time
+# Real variables TODO Enable
+WELCOME_TIME_S = 30
 # SCENARIO_TIME_S = 60
 # QUESTION_TIME_S = 5 * 60
 # BREAK_TIME_S = 10 * 60
 
 PROGRESS_BAR_MAX_ITERATIONS = 80
 
+def display_welcome
+    system "clear"
+    puts "Welcome to the Casper simulation.".colorize(:yellow)
+    puts "There will be #{NUM_QUESTIONS_TOTAL} total questions.".colorize(:cyan)
+    puts "There will be a break after #{NUM_QUESTIONS_PER_PERIOD} questions."
+        .colorize(:cyan)
+    puts "\n"
+    display_progress_bar(5)
+end
+
 def display_scenario(index)
+    system "clear"
     puts "This is the scenario #{index.to_s}"
     display_progress_bar(SCENARIO_TIME_S)
 end
 
-def display_questions
-    puts "These are the questions"
+def display_questions(index)
+    system "clear"
+    puts "These are the questions for scenario #{index.to_s}"
     display_progress_bar(QUESTION_TIME_S)
 end
 
 def display_break
+    system "clear"
     puts "This is a break"
     display_progress_bar(BREAK_TIME_S)
 end
@@ -34,7 +50,7 @@ def display_progress_bar(total_time_s)
     progressbar_max_iterations = 80;
     progressbar = ProgressBar.create(
         :total => progressbar_max_iterations,
-        :title => "Time Left", 
+        :title => "Time Left On This Screen", 
         :progress_mark => " ", 
         :remainder_mark => "=")
     start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
@@ -48,12 +64,15 @@ def display_progress_bar(total_time_s)
     progressbar.finish
 end
 
-global_scenario_index = 1
-while global_scenario_index <= NUM_QUESTIONS_TOTAL do
+# Main
+
+display_welcome
+scenario_index = 1
+while scenario_index <= NUM_QUESTIONS_TOTAL do
     (1..NUM_QUESTIONS_PER_PERIOD).each do |_|
-        display_scenario(global_scenario_index)
-        display_questions
-        global_scenario_index += 1
+        display_scenario(scenario_index)
+        display_questions(scenario_index)
+        scenario_index += 1
     end
     display_break
 end
